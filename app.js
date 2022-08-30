40bc7ef3f606df2ad207852e3cb74e635f64b4e8
Ori Nagar <ori.ngr@gmail.com>
2022-08-30 17:59:20 +0300

    setting headers to solve CORS error

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const quotesRoutes = require("./routes/quotes-routes");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use(quotesRoutes);
 
 app.use((error, req, res, next) => {
